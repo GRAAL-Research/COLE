e62bf1508e4149f38ce6f0d0254781ab702b168d
MedAmineYoussef <MAYOU29@ulaval.ca>
2025-06-09 16:32:04 -0400

    Update page.js

--- colle_nlu_benchmark_site/src/app/benchmarks/page.js
+++ colle_nlu_benchmark_site/src/app/benchmarks/page.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 export default function benchmarks(){
     return (
         <div>
@@ -10,47 +11,66 @@ export default function benchmarks(){
     <div className="space-y-8">
 
     <Benchmark title="Allo-ciné.ca"
+               link="https://huggingface.co/datasets/CATIE-AQ/allocine_fr_prompt_sentiment_analysis"
                description="Allo-ciné tests language understanding in sentiment classification
             by feeding movie reviews which can be either positive and negatve, the task consists
             in giving the correct sentiment for each review.
             "
                metrics="Accuracy"></Benchmark>
         <Benchmark title="XNLI - Cross lingual sentence representation"
+                   link="https://github.com/facebookresearch/XNLI"
                description="This task consists of pairs of sentences where the goal is to determine the relation between the two sentences, this relation can be either
                             entailement, neutral or contradiction."
                metrics="Accuracy"></Benchmark>
 
         <Benchmark title="FrCola - a French Corpus of Linguistic Acceptability Judge"
+                   link="https://github.com/davebulaval/qfrcola"
                    description="FrCola is a french dataset made from multiple french language sites such as académie-française.fr and vitrinelinguistique.com.
                    It aims to tests models ability to determine a sentence's acceptability in french on subjects such as grammar and syntax. The answer is a binary label
                    indicating if the sentence is correct or not."
                   metrics="MatthewsCC"></Benchmark>
 
         <Benchmark title="FQuad - French question answering dataset"
+                   link="https://arxiv.org/pdf/2002.06071"
                description="Fquad is question/answer pair built on high-quality wikipedia articles. The goal of the model in this task is to accurately predict if the answer to the
             question really can be found in the provided answer."
                metrics="Pearson,Spearman"></Benchmark>
 
         <Benchmark title="FR-Blimp - Linguistic minimal pairs"
+                   link="https://github.com/davebulaval/FrBLiMP"
                description="This task gives the model sentences pairs, the goal is to determine if the sentences
             are semantically equivalent, or, put more simply, if they mean the same thing, even with slightly different
             syntax and words."
                metrics="Accuracy"></Benchmark>
 
         <Benchmark title="Opus Parcus - Open Subtitles Paraphrase Corpus"
+                   link="https://huggingface.co/datasets/GEM/opusparcus"
                description="Opus parcus, built with data from Open Subtitles, consists of pairs of sentences with a similar or quasi identical semantic meaning. When testing, a label is provided
             being a score of 1 to 5, with 1 meaning 2 sentences bearing no semantic similarity, and 5, 2 sentences that mean the same thing exactly."
                metrics="Pearson,Spearman"></Benchmark>
         <Benchmark title="Paws-X - A Cross-lingual Adversarial Dataset for Paraphrase Identification "
+                   link="https://paperswithcode.com/dataset/paws-x"
                description="This task aims to test paraphrase identification by giving two sentences and a label defining if these sentences are equivalent in meaning or not."
                metrics="Accuracy"></Benchmark>
         <Benchmark title="Piaf - The French-language dataset of Questions-Answers"
+                   link="https://aclanthology.org/2020.lrec-1.673/"
                description="This task consists of pairs of questions and text answers with information of where in the answer is the truly relevant information."
                metrics="Pearson,Spearman"></Benchmark>
         <Benchmark title="Sick-FR - French Sentences Involving Compositional Knowledge"
+                   link=" https://huggingface.co/datasets/Lajavaness/SICK-fr"
                description="This task also has pairs of sentences and notes them on 2 dimensions, relatedness and entailment. https://paperswithcode.com/dataset/sick, While relatedness scales from 1 to 5,
             entailement is a choice between entails, contradicts or neutral."
                metrics="Pearson,Spearman"></Benchmark>
+        <Benchmark title="Sts22-Crosslingual - Multilingual News Article Similarity"
+                   link="https://huggingface.co/datasets/mteb/sts22-crosslingual-sts/viewer/fr"
+               description="This task evaluates whether pairs of news articles, written in different languages, cover the same story. It focuses on document-level similarity,
+                where systems rate article pairs on a 4-point scale from most to least similar"
+               metrics="Pearson,Spearman"></Benchmark>
+        <Benchmark title="Gqnli-Fr - The Generalized Quantifier NLI Challenge Dataset"
+                   link="https://huggingface.co/datasets/maximoss/gqnli-fr"
+               description="The dataset consists of carefully constructed premise-hypothesis pairs that involve quantifier logic (e.g., most, at least, more than half). The goal is to evaluate the model's ability to reason about these expressions and determine whether the hypothesis logically follows from the premise, contradicts it, or is neutral."
+               metrics="Accuracy"></Benchmark>
+
 
     </div>
 
@@ -59,11 +79,22 @@ export default function benchmarks(){
 
     );
 };
-function Benchmark({ title, description, metrics }) {
+function Benchmark({ title, description, metrics, link }) {
   return (
     <div className="p-6 bg-white border border-gray-200 rounded-lg shadow-sm">
-      <h3 className="text-xl font-semibold text-blue-700 mb-2 pb-1 border-b-2 border-blue-500 inline-block">{title}</h3>
+      <h3 className="text-xl font-semibold text-blue-700 mb-2 border-b-2 border-blue-500 inline-block">
+        {link ? (
+          <Link href={link} className="hover:underline">
+            {title}
+          </Link>
+        ) : (
+          title
+        )}
+      </h3>
       <p className="text-gray-700 mb-2">{description}</p>
-      <p className="text-sm text-gray-500"><span className="font-medium">Metrics:</span> {metrics}</p>
+      <p className="text-sm text-gray-500">
+        <span className="font-medium">Metrics:</span> {metrics}
+      </p>
     </div>
-  );}
+  );
+}
