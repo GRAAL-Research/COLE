5d2641389b66fc51d9df6274c7f9f42bf92c4423
Yurhuval <yanyantremblay@gmail.com>
2025-07-11 10:10:06 -0400

    added tests to model_evaluator

--- tests/model_evaluation/test_model_evaluator.py
+++ tests/model_evaluation/test_model_evaluator.py
@@ -1,12 +1,15 @@
 import json
 from unittest import TestCase
 
+from src.dataset.dataset import Dataset
 from src.evaluation.model_evaluator import ModelEvaluator
 from src.model.model import Model
 from src.task.task_factory import tasks_factory
 
 MODEL_NAME = "a model"
 preds = ["0", "0", "0", "0"]
+gen = ["1", "1", "1", "1"]
+BASE_TASK_NAME = "qfrcola"
 
 
 class ModelEvaluatorTest(TestCase):
@@ -14,9 +17,9 @@ class ModelEvaluatorTest(TestCase):
     def setUp(self):
         self.model = Model(MODEL_NAME)
         self.model.infer = lambda *args, **kwargs: preds
-        self.model.generate = lambda *args, **kwargs: ["0", "0", "0"]
+        self.model.generate = lambda *args, **kwargs: gen
         self.tester = ModelEvaluator()
-        self.tasks = tasks_factory(["qfrcola"])
+        self.tasks = tasks_factory([BASE_TASK_NAME])
 
     def test_when_evaluating_return_formatted_dict(self):
         ret = self.tester.evaluate(self.model, self.tasks)
@@ -40,9 +43,19 @@ class ModelEvaluatorTest(TestCase):
                     "qfrcola": {
                         "accuracy": {
                             "accuracy": 0.5,
-                            f"accuracy_warning": f"Your prediction size is of {len(preds)}, while the ground truths size is of {len(self.tasks[0].dataset.ground_truths)}. We computed the metric over the first {len(preds)} elements.",
+                            f"accuracy_warning": f"Your prediction size is of '{len(preds)}', while the ground truths size is of '{len(self.tasks[0].dataset.ground_truths)}'. We computed the metric over the first {len(preds)} elements.",
                         }
                     }
                 }
             ],
         }
+
+    def test_when_task_is_generative_generate(self):
+        TASK_NAME = "fquad"
+        tasks = tasks_factory([TASK_NAME])
+        predictions = self.tester.evaluate(self.model, tasks)
+        assert predictions["tasks"] == [{TASK_NAME: gen}]
+
+    def test_when_task_is_inference_infer(self):
+        ret = self.tester.evaluate(self.model, self.tasks)
+        assert ret["tasks"] == [{BASE_TASK_NAME: preds}]
