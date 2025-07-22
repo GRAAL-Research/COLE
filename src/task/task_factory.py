4a137fa03bd082511178b668af02eb9d4ea6f646
MedAmineYoussef <MAYOU29@ulaval.ca>
2025-07-22 15:58:08 -0400

    rte3-french added

--- src/task/task_factory.py
+++ src/task/task_factory.py
@@ -143,6 +143,14 @@ def tasks_factory(task_names: Union[Dict, List[str]]) -> List[Task]:
                         task_type=TaskType.INFERENCE,
                     )
                 )
+            case "rte3-french":
+                tasks.append(
+                    Task(
+                        task_name=task_name,
+                        metric="accuracy",
+                        task_type=TaskType.INFERENCE,
+                    )
+                )
             case _:
                 error = f"Unknown task {task_name}."
                 logging.error(error)
